907bdc161e9c285d5e5d231080e27e7e3ec7effc
Oliver Warrick <oliver.warrick@thedropbears.org.au>
2019-01-14 15:57:16 +1100

    slightly less broken

--- tapeVision.py
+++ tapeVision.py
@@ -6,31 +6,38 @@ import math
 
 def findTape(img):
     minGreen = (70, 10, 50)
-    maxGreen = (120, 255, 253)
+    maxGreen = (120, 255, 254)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, minGreen, maxGreen)
+    mask=cv2.erode(mask, None, iterations=2)
+    mask=cv2.dilate(mask, None, iterations=2)
     
     contours = cv2.findContours(mask, 1,2)[-2]
-    removeUnder(10)
     if len(contours)>1:
         contours.sort(key=cv2.contourArea)
-        rects = (cv2.minAreaRect(contours[-1]), cv2.minAreaRect(contours[-2]))
-        boxes = (cv2.boxPoints(rects[-1]), cv2.boxPoints(rects[-2]))
+        removeUnder(50, contours)
+        rects=[]
+        boxes=[]
+        for i in range(len(contours)):
+            rects.append(cv2.minAreaRect(contours[i]))
+            print(rects[len(rects)-1][2])
+            boxes.append(cv2.boxPoints(rects[i]))
 
-    boxes=np.int0(boxes)
-    show=currentImg.copy()
-    cv2.drawContours(show, [boxes[0]] ,0,  (0,0,255) ,2)
-    cv2.drawContours(show, [boxes[1]] ,0,  (0,0,255) ,2)
-    cv2.imshow("mask", mask)
-    cv2.imshow("img", show)
+        boxes=np.int0(boxes)
+        show=currentImg.copy()
+        for i in range(len(boxes)):
+            cv2.drawContours(show, [boxes[i]] ,0,  (0,0,255) ,2)
+        cv2.imshow("mask", mask)
+        cv2.imshow("img", show)
         
-    return {"rects" : rects, "boxes" : boxes}
+        return {"rects" : rects, "boxes" : boxes}
 
-def removeUnder(under):
+def removeUnder(under, contours):
     for c in range(len(contours)):
-        if contours[c].contoursArea<under:
+        if cv2.contourArea(contours[c])<under:
+            print("removed")
             del contours[c]
-            removeUnder(under)
+            removeUnder(under, contours)
             break
         
 def findMid(box):
@@ -46,9 +53,14 @@ def findAngleToCamera(FOV, X, W):
     angFromL=perc*FOV
     return angFromL-FOV/2
 
-def rotCheck(boxs):
-    boxs[1].sort(key=lambda a : a[1])
+def findPairs(rects):
+    rects.sorted(key=lambda x:x[2])
+    for c in range(len(rects)):
+        for i in range(len(rects)):
+            if round(rects[c][2]-60)/5) == round(rects[c][2])/5):
+                
 
+#( center (x,y), (width, height), angle of rotation ) min area rect things
 
 files = os.listdir(".\images")
 index = 0
@@ -56,8 +68,8 @@ currentImg = cv2.imread("./images/"+files[index], 1)
 
 while True:
     tapes = findTape(currentImg)
-    mid = findMid(tapes["boxes"])
-    angle = findAngleToCamera(90, mid[2][0], currentImg.shape[0])
+    #mid = findMid(tapes["boxes"])
+    #angle = findAngleToCamera(90, mid[2][0], currentImg.shape[0])
     #print(angle)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -65,7 +77,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('n'):
         index+=1
         currentImg = cv2.imread("./images/"+files[index], 1)
-        print("swapped")
+        #print("swapped")
 
 #cap.release()
 cv2.destroyAllWindows()
